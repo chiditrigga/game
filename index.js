0b543a19f9a5d17fb30a3d76201c63b3e33b72b4
chiditrigga <105122478+chiditrigga@users.noreply.github.com>
2022-07-31 01:42:27 +0100

    start

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let guess = window.prompt('what')
+
+let config = (guess)=>{
+    alert(guess)
+}
+
+let list = [1,2]
+let randomElement = list[select()]
+function select() {
+    let random = Math.random() * list.length;
+    return Math.floor(random);
+  }
+
+
+
+while(guess == list.randomElement){
+config(guess)
+
+
+
+}
+
